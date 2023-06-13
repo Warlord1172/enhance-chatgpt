2ecb11f67d060cd5468daafe85cd92778ebd965c
Warlord1172 <damiangonzalez604@gmail.com>
2023-06-12 20:12:41 -0500

    update #061223

--- src/codeBlock.js
+++ src/codeBlock.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { solarizedlight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import React from "react";
+//codeBlock section
 const CodeBlock = ({ code, language }) => {
   const copyCodeToClipboard = () => {
     navigator.clipboard.writeText(code);
   };
-
   return (
     <div className="message">
       <div className="code-block">
         <div className="code-block-header">
           <span className="language">{language}</span>
-          <button className="copy-button" onClick={copyCodeToClipboard}>Copy Code</button>
+          <button className="copy-button" onClick={copyCodeToClipboard}>
+            Copy Code
+          </button>
         </div>
         <SyntaxHighlighter language={language} style={solarizedlight}>
           {code}
