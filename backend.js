37d52fbae68ae2c76df209677e18c4632cbffedd
Warlord1172 <damiangonzalez604@gmail.com>
2023-06-22 11:53:41 -0500

    port 1

--- backend.js
+++ backend.js
@@ -5,8 +5,7 @@ const https = require("https");
 const app = express();
 const path = require('path');
 // Set the PORT environment variable to a custom value
-process.env.PORT = '10080';
-const port  = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 /*
 //google server side
