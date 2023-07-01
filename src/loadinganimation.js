8ee4fbc3bbb7a3027317b36310d03c8fd577a6e2
Warlord1172 <damiangonzalez604@gmail.com>
2023-07-01 11:11:57 -0500

    more changes

--- src/loadinganimation.js
+++ src/loadinganimation.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 const Loading = () => {
   return (
     <div className="main-loading-container">
-      <div className="main-loading-animation">  
+      <div className="main-loading-animation">
         <span></span>
         <span></span>
         <span></span>
