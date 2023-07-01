ce7908a643741cdb8204c090b46566a839dd268b
Warlord1172 <damiangonzalez604@gmail.com>
2023-07-01 11:06:59 -0500

    changed animation

--- src/loadinganimation.js
+++ src/loadinganimation.js
@@ -4,9 +4,13 @@ import React, { useState, useEffect } from 'react';
 const Loading = () => {
   return (
     <div className="main-loading-container">
-      <div className="main-loading-animation">
-        <div className="main-loading-text">Deploying Application, please wait.</div>
+      <div className="main-loading-animation">  
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
+      <div className="main-loading-text">Deploying Application, please wait.</div>
     </div>
   );
 };
