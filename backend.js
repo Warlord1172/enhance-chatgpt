0dadaa711edd8a6d27fc5d018912597e20e3005a
Warlord1172 <damiangonzalez604@gmail.com>
2023-07-09 00:28:25 -0500

    models not available yet

--- backend.js
+++ backend.js
@@ -170,7 +170,7 @@ const getModels = (Key) => {
       response.on("end", () => {
         try {
           // A list of available models
-          const availableModels = ["gpt-3.5-turbo-0301","gpt-3.5-turbo",   "gpt-3.5-turbo-16k-0613","gpt-3.5-turbo-0613", "gpt-3.5-turbo-16k","gpt-4","gpt-4-0613","gpt-4-0314","gpt-4-32k","gpt-4-32k-0613"];
+          const availableModels = ["gpt-3.5-turbo-0301","gpt-3.5-turbo",   "gpt-3.5-turbo-16k-0613","gpt-3.5-turbo-0613", "gpt-3.5-turbo-16k","gpt-4","gpt-4-0613","gpt-4-0314"];
           const parsedData = JSON.parse(data);
           const engines = parsedData.data;
           const deprecatedModelsList = engines.filter(engine => !availableModels.includes(engine.id));
