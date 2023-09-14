cc0649b59742196bdcb65f3703538e6702ff33a2
Dgon06utrgv <Damian.gonzalez06@utrgv.edu>
2023-09-14 08:16:51 -0500

    update #091423 updated backend and frontend.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    /* app.use(
+    app.use(
     '/api',
     createProxyMiddleware({
         target: 'https://chatgpt-unleashed.onrender.com:10080',
         changeOrigin: true,
     })
     );
-*/
+    /*
     app.use(
         '/API',
         createProxyMiddleware({
@@ -16,4 +16,5 @@ module.exports = function(app) {
         changeOrigin: true,
     })
     ) 
+    */
 };
