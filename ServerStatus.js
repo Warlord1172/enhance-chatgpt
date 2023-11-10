40670d8d7ccd55be3bb41472b93cb74995bd077b
Dgon06utrgv <Damian.gonzalez06@utrgv.edu>
2023-11-10 09:23:05 -0600

    added server status update #111023

---
+++ ServerStatus.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function OpenAIStatusTracker() {
+  const [apiName, setApiName] = useState("");
+  const [apiStatus, setApiStatus] = useState("");
+  const [apiUptime, setApiUptime] = useState("");
+
+  useEffect(() => {
+    const fetchStatus = async () => {
+      try {
+        const response = await axios.get("https://status.openai.com/");
+        const parser = new DOMParser();
+        const htmlDoc = parser.parseFromString(response.data, "text/html");
+        const apiStatusElement = htmlDoc.querySelector(
+          'div[data-component-id="scd0s93nldpb"]'
+        );
+        if (apiStatusElement) {
+          const name = apiStatusElement
+            .querySelector(".name")
+            .textContent.trim();
+          const status = apiStatusElement
+            .querySelector(".component-status")
+            .textContent.trim();
+          const uptime = apiStatusElement
+            .querySelector("#uptime-percent-scd0s93nldpb var")
+            .textContent.trim();
+          setApiName(name);
+          setApiStatus(status);
+          setApiUptime(uptime);
+        }
+      } catch (error) {
+        console.error("Error fetching API status:", error);
+      }
+    };
+
+    fetchStatus();
+    const interval = setInterval(fetchStatus, 60000); // Fetch status every minute
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
+  return (
+    <div className="openai-status-tracker">
+      <h2>OpenAI API Status</h2>
+      <p>Name: {apiName}</p>
+      <p>Status: {apiStatus}</p>
+      <p>Uptime: {apiUptime}%</p>
+    </div>
+  );
+}
+
+export default OpenAIStatusTracker;
--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import parse from 'html-react-parser';
 import fetch from "node-fetch";
 import { v4 as uuidv4 } from "uuid";
 import Loading from "./loadinganimation";
+import OpenAIStatusTracker from './ServerStatus';
 //import fs from 'fs';
 //import axios from 'axios';
 // Main application component
@@ -659,6 +660,7 @@ function App() {
         {
           //<GoogleSignInButton />
         }
+         <OpenAIStatusTracker />
       </aside>
       )}
       {/* Hamburger menu button */}
