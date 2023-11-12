932ea0d4b68163bb3a5828a4e0d2443fdde33079
Warlord1172 <damiangonzalez604@gmail.com>
2023-11-12 17:41:18 -0600

    fixed bug

--- src/windowcloseprompt.js
+++ src/windowcloseprompt.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal } from "react-bootstrap";
 
 const WindowClosePrompt = ({ message, onConfirm }) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleConfirm = () => {
     setIsOpen(false);
@@ -11,18 +11,35 @@ const WindowClosePrompt = ({ message, onConfirm }) => {
 
   const handleClose = () => {
     setIsOpen(false);
-    window.close(); // Force close the window
   };
 
-  return (
-    <Modal show={isOpen} onHide={handleClose} backdrop="static">
-      <Modal.Body>{message}</Modal.Body>
-      <Modal.Footer>
-        <button onClick={handleConfirm}>Yes</button>
-        <button onClick={handleClose}>No</button>
-      </Modal.Footer>
-    </Modal>
-  );
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = message;
+      setIsOpen(true);
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [message, onConfirm]);
+
+  if (isOpen) {
+    return (
+      <Modal show={isOpen} onHide={() => {}} backdrop="static">
+        <Modal.Body>{message}</Modal.Body>
+        <Modal.Footer>
+          <button onClick={handleConfirm}>Yes</button>
+          <button onClick={handleClose}>No</button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  return null;
 };
 
 export default WindowClosePrompt;
